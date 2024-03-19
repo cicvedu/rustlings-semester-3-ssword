1c4feeaa59b2fd3bc37273958ed90e8b64524d82
ssword <ssword@gmail.com>
2024-03-19 12:43:21 +0800

    Revert "finally"
    This reverts commit 4661abf6c30e6771683ad42eaccf07ae5b0b4a84.

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -10,12 +10,15 @@ fn main() {
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
         .as_secs(); // What's the use of this timestamp here?
-    let your_command = format!("rustc-env=TEST_FOO={}", timestamp);
+    let your_command = format!(
+        "Your command here with {}, please checkout exercises/tests/build.rs",
+        timestamp
+    );
     println!("cargo:{}", your_command);
 
     // In tests8, we should enable "pass" feature to make the
     // testcase return early. Fill in the command to tell
     // Cargo about that.
-    let your_command = "rustc-cfg=feature=\"pass\"";
+    let your_command = "Your command here, please checkout exercises/tests/build.rs";
     println!("cargo:{}", your_command);
 }
--- exercises/tests/tests6.rs
+++ exercises/tests/tests6.rs
@@ -7,6 +7,8 @@
 // Execute `rustlings hint tests6` or use the `hint` watch subcommand for a
 // hint.
 
+// I AM NOT DONE
+
 struct Foo {
     a: u128,
     b: Option<String>,
@@ -18,9 +20,8 @@ struct Foo {
 unsafe fn raw_pointer_to_box(ptr: *mut Foo) -> Box<Foo> {
     // SAFETY: The `ptr` contains an owned box of `Foo` by contract. We
     // simply reconstruct the box from that pointer.
-    let mut ret: Box<Foo> = unsafe { Box::from_raw(ptr as *mut Foo) };
-    ret.b = Some("hello".to_owned());
-    ret
+    let mut ret: Box<Foo> = unsafe { ??? };
+    todo!("The rest of the code goes here")
 }
 
 #[cfg(test)]
